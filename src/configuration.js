d56b7779646e337d030bb873b7b1cf328e08fa07
Efe Çağın Kaptan <efekaptan@gmail.com>
2017-07-24 08:52:32 +0300

    production api url

--- src/configuration.js
+++ src/configuration.js
@@ -1,7 +1,7 @@
 const configuration = {};
 
 if (process.env.NODE_ENV === 'production') {
-    configuration.apiUrl = 'https://galatia/api';
+    configuration.apiUrl = 'https://arcane-caverns-50573.herokuapp.com/api';
 } else {
     configuration.apiUrl = 'http://localhost:3000/api';
 }
